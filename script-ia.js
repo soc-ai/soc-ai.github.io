fe48f53fc6d1b7abe9735dca3baa73b2f9af37b8
soc-ai <socialai@tuta.io>
2025-03-10 16:29:37 -0400

    Delete script-ia.js

--- script-ia.js
+++
@@ -1,4 +0,0 @@
-let encodedUrl = "aHR0cHM6Ly9pZnJhbWUuaW50ZXJheGFpLmNvbS82NzlmOWNlZmIxMzk2MTM4OGU0N2Q4YWY="; 
-
-let decodedUrl = atob(encodedUrl);
-document.getElementById("myIframe").src = decodedUrl;
